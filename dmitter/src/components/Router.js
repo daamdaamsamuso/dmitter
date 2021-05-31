2f3e5f38ad98f9f384bf4eb5d549a4f20abc608a
promoted.go@daamdaam.co.kr <promoted.go@gmail.com>
2021-05-31 14:31:14 +0900

    add router

---
+++ dmitter/src/components/Router.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Route, HashRouter as Router, Switch } from "react-router-dom";
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
+
+const AppRouter = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <Router>
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
